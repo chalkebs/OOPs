c8c6091b8970657024df70e0a699608c1ae45926
chalkebs <51581618+chalkebs@users.noreply.github.com>
2019-07-11 15:10:12 +0530

    Add files via upload

---
+++ InventoryJson.java
@@ -0,0 +1,63 @@
+package com.bridgelab.oops;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class InventoryJson 
+{
+	public static void main(String[]args)
+	{
+		 JSONParser jsonParser = new JSONParser();
+         JSONArray array=new  JSONArray();
+         JSONObject object=new  JSONObject();
+	        
+         try 
+	        {
+	            //Read JSON file
+	            object = (JSONObject) jsonParser.parse(new FileReader("/home/admin28/eclipse-workspace/BridgelabzJavaCode/src/inventoryJson.json"));
+	 
+	            array = (JSONArray)object.get("Rice");
+	            System.out.print("For Rice: ");
+	            inventoryCalculation(array);
+	            System.out.println();
+	            
+	            array = (JSONArray)object.get("Wheat");
+	    		System.out.print("For Wheat: ");
+	    		inventoryCalculation(array);
+	    		System.out.println();
+	    		
+	            array = (JSONArray)object.get("Pulses");
+	    		System.out.print("For Pulses: ");
+	    		inventoryCalculation(array);
+	    		System.out.println();
+	  
+	        } catch (FileNotFoundException e) 
+         	{
+	            e.printStackTrace();
+	        } catch (IOException e) 
+         	{
+	            e.printStackTrace();
+	        } catch (Exception e) 
+         	{
+	        	e.printStackTrace();
+	        }
+	 }
+	
+	 public static void inventoryCalculation(JSONArray array)
+	    {
+		 	Iterator<?> iterator = array.iterator();	//	iterator to iterate
+		 	while(iterator.hasNext()) 
+		 	{
+		 		JSONObject obj = (JSONObject)iterator.next();
+		 		long weight = (long)obj.get("weight");
+		 		long price = (long)obj.get("price");
+		 		System.out.print(weight * price);
+		 	}
+	    }
+}
